ec558e8056f511ca71372a1f15468d6919a87036
stevie-mccomb <stevie.mccomb@gmail.com>
2013-11-05 16:14:16 -0700

    Finished homepage and annotated PSD

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 var app = angular.module('hasturs', [])
+	.config(viewRouter);
+
+// Router
+
+function viewRouter($routeProvider) {
+	$routeProvider
+		.when('/products', {templateUrl: 'partials/products.html'})
+		.otherwise({templateUrl: 'partials/home.html'});
+}
+
+// Directives
+
+app.directive('navigation', ['$rootScope', function($rootScope) {
+	return {
+		link: function(scope, elem) {
+			scope.activeView = 'home';
+			scope.setActive = function(viewName) {
+				scope.activeView = viewName;
+			}
+		}
+	}
+}]);
+
+app.directive('productsList', function() {
+	return {
+		link: function(scope, elem) {
+			console.log(elem[0].offsetHeight);
+		}
+	}
+});
---
+++ js/controllers.js
@@ -0,0 +1,5 @@
+// Controllers
+
+app.controller('NavCtrl', ['$rootScope', function($rootScope) {
+	
+}]);
