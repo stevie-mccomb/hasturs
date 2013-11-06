b43d222f42b9139689dc001beb8400854834ebb3
Stevie McComb <stevie.mccomb@gmail.com>
2013-11-05 21:57:53 -0700

    Added products and contact page.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('hasturs', [])
+var app = angular.module('hasturs', ['ngResource'])
 	.config(viewRouter);
 
 // Router
@@ -9,7 +9,7 @@ function viewRouter($routeProvider) {
 		.when('/products', {templateUrl: 'partials/products.html'})
 		.when('/contact', {templateUrl: 'partials/contact.html'})
 		.otherwise({templateUrl: 'partials/home.html'});
-}
+};
 
 // Directives
 
@@ -24,10 +24,72 @@ app.directive('navigation', ['$rootScope', function($rootScope) {
 	}
 }]);
 
-app.directive('productsList', function() {
+app.directive('slideshow', ['SlideFactory', function(SlideFactory) {
 	return {
 		link: function(scope, elem) {
-			console.log(elem[0].offsetHeight);
+			scope.slides = SlideFactory.slides.get();
+			scope.current = 0;
+
+			scope.slidesLength = function(obj) {
+			    var size = 0, key;
+			    for (key in obj) {
+			        if (obj.hasOwnProperty(key)) size++;
+			    }
+			    return size;
+			};
+
+			scope.previous = function() {
+				if (scope.current - 1 < 0) {
+					scope.current = scope.slidesLength(scope.slides) - 1;
+				} else {
+					scope.current--;
+				}
+			}
+
+			scope.next = function() {
+				if (scope.current + 1 > scope.slidesLength(scope.slides) - 1) {
+					scope.current = 0;
+				} else {
+					scope.current++;
+				}
+			}
 		}
 	}
-});
+}]);
+
+app.directive('featuredEvents', ['EventFactory', function(EventFactory) {
+	return {
+		link: function(scope, elem) {
+			scope.events = EventFactory.events.get();
+		}
+	}
+}]);
+
+app.directive('featuredProducts', ['ProductFactory', function(ProductFactory) {
+	return {
+		link: function(scope, elem) {
+			scope.productsOne = ProductFactory.productsOne.get();
+			scope.productsTwo = ProductFactory.productsTwo.get();
+		}
+	}
+}]);
+
+app.directive('productContainer', ['ProductFactory', function(ProductFactory) {
+	return {
+		link: function(scope, elem) {
+			scope.viewingCategory = false;
+			scope.viewingProduct = false;
+			scope.productCategories = ProductFactory.productCategories.get();
+
+			scope.viewCategory = function(category) {
+				scope.category = category;
+				scope.products = ProductFactory.products.get();
+				scope.viewingCategory = category.name;
+			}
+
+			scope.viewProduct = function(product) {
+				scope.viewingProduct = product;
+			}
+		}
+	}
+}]);
---
+++ js/services.js
@@ -0,0 +1,22 @@
+// Services
+
+app.factory('SlideFactory', ['$resource', function($resource) {
+	return {
+		slides: $resource('data/slides.json')
+	}
+}]);
+
+app.factory('EventFactory', ['$resource', function($resource) {
+	return {
+		events: $resource('data/featured-events.json')
+	}
+}]);
+
+app.factory('ProductFactory', ['$resource', function($resource) {
+	return {
+		productCategories: $resource('data/product-categories.json'),
+		products: $resource('data/products.json'),
+		productsOne: $resource('data/featured-products-one.json'),
+		productsTwo: $resource('data/featured-products-two.json')
+	}
+}]);
